d0d92c87e5057219760b93c61a18c36c8f62317f
Aiden Grossman <agrossman154@yahoo.com>
2023-06-22 21:14:18 +0000

    Switch command filter from empty string to None
    When using an empty string in the command filter during IR extraction,
    ml-compiler-opt still tries to assume we're filtering for something.
    This patch changes the CMake builder (currently the only builder) to
    pass in None so that ml-compiler-opt knows that no compiler option
    filtering is occurring.

--- llvm_ir_dataset_utils/builders/cmake_builder.py
+++ llvm_ir_dataset_utils/builders/cmake_builder.py
@@ -41,6 +41,6 @@ def extract_ir(build_dir, corpus_dir):
     objects = extract_ir_lib.load_from_compile_commands(
         json.load(compilation_command_db_file), corpus_dir)
   relative_output_paths = extract_ir_lib.run_extraction(
-      objects, multiprocessing.cpu_count(), "llvm-objcopy", "", None,
+      objects, multiprocessing.cpu_count(), "llvm-objcopy", None, None,
       ".llvmcmd", ".llvmbc")
   extract_ir_lib.write_corpus_manifest(None, relative_output_paths, corpus_dir)
