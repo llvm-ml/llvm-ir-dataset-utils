fb8e652833ee738bdc4fa2c8214314150c6a21a1
Aiden Grossman <agrossman154@yahoo.com>
2023-07-03 00:30:26 -0700

    Format get_build_failre_logs.py
    Forgot to format it when checking it in initially.

--- llvm_ir_dataset_utils/tools/get_build_failure_logs.py
+++ llvm_ir_dataset_utils/tools/get_build_failure_logs.py
@@ -10,17 +10,21 @@ from absl import flags
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_string('corpus_dir', None, 'The corpus directory to look for build logs in.')
+flags.DEFINE_string('corpus_dir', None,
+                    'The corpus directory to look for build logs in.')
 
 flags.mark_flag_as_required('corpus_dir')
 
+
 def main(_):
-  for build_manifest_file_path in glob.glob(os.path.join(FLAGS.corpus_dir, '*/build_manifest.json')):
+  for build_manifest_file_path in glob.glob(
+      os.path.join(FLAGS.corpus_dir, '*/build_manifest.json')):
     with open(build_manifest_file_path) as build_manifest_file:
       build_manifest = json.load(build_manifest_file)
     for target in build_manifest['targets']:
       if target['success'] == False:
         print(target['name'] + ',' + target['build_log'])
+
 
 if __name__ == '__main__':
   app.run(main)
