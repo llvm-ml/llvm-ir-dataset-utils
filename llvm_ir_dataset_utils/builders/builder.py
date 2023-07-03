e0e69d154669e5fd2ab7cded3f324bc2f2743c90
Aiden Grossman <39388941+boomanaiden154@users.noreply.github.com>
2023-07-03 16:13:01 -0700

    Make git log to a file (#34)
    This pollutes the logs less and makes it easier to investigate things
    after the fact.
    
    Also should fix #26.

--- llvm_ir_dataset_utils/builders/builder.py
+++ llvm_ir_dataset_utils/builders/builder.py
@@ -16,39 +16,50 @@ from llvm_ir_dataset_utils.builders import cargo_builder
 THREADS = multiprocessing.cpu_count()
 
 
-def download_source_code_git(repo_url, repo_name, commit_sha, base_dir):
+def download_source_code_git(repo_url, repo_name, commit_sha, base_dir,
+                             corpus_dir):
   # If the directory already exists, we can skip downloading the source,
   # currently just assuming that the requested commit is present
   if not os.path.exists(os.path.join(base_dir, repo_name)):
-    git_command_vector = ["git", "clone", repo_url]
-    if commit_sha is None or commit_sha == '':
-      git_command_vector.append('--depth=1')
-    git_command_vector.append(repo_name)
-    logging.info(f"Cloning git repository {repo_url}")
-    environment = os.environ.copy()
-    environment['GIT_TERMINAL_PROMPT'] = '0'
-    environment['GIT_ASKPASS'] = 'echo'
-    subprocess.run(git_command_vector, cwd=base_dir, env=environment)
-  if commit_sha is not None and commit_sha != '':
-    commit_checkout_vector = ["git", "checkout", commit_sha]
-    logging.info(f"Checked out commit SHA {commit_sha}")
-    subprocess.run(
-        commit_checkout_vector, cwd=os.path.join(base_dir, repo_name))
+    with open(os.path.join(corpus_dir, 'git.log'), 'w') as git_log_file:
+      git_command_vector = ["git", "clone", repo_url]
+      if commit_sha is None or commit_sha == '':
+        git_command_vector.append('--depth=1')
+      git_command_vector.append(repo_name)
+      logging.info(f"Cloning git repository {repo_url}")
+      environment = os.environ.copy()
+      environment['GIT_TERMINAL_PROMPT'] = '0'
+      environment['GIT_ASKPASS'] = 'echo'
+      subprocess.run(
+          git_command_vector,
+          cwd=base_dir,
+          stdout=git_log_file,
+          stderr=git_log_file,
+          env=environment)
+    if commit_sha is not None and commit_sha != '':
+      commit_checkout_vector = ["git", "checkout", commit_sha]
+      logging.info(f"Checked out commit SHA {commit_sha}")
+      subprocess.run(
+          commit_checkout_vector,
+          cwd=os.path.join(base_dir, repo_name),
+          stdout=git_log_file,
+          stderr=git_log_file)
 
 
 def parse_and_build_from_description(corpus_description, base_dir,
                                      corpus_base_dir):
+  corpus_dir = os.path.join(corpus_base_dir, corpus_description["repo_name"])
+  pathlib.Path(corpus_dir).mkdir(exist_ok=True, parents=True)
   if not os.path.exists(base_dir):
     os.makedirs(base_dir)
   download_source_code_git(corpus_description["git_repo"],
                            corpus_description["repo_name"],
-                           corpus_description["commit_sha"], base_dir)
+                           corpus_description["commit_sha"], base_dir,
+                           corpus_dir)
   build_dir = os.path.join(base_dir, corpus_description["repo_name"] + "-build")
   if not os.path.exists(build_dir):
     os.makedirs(build_dir)
   source_dir = os.path.join(base_dir, corpus_description["repo_name"])
-  corpus_dir = os.path.join(corpus_base_dir, corpus_description["repo_name"])
-  pathlib.Path(corpus_dir).mkdir(exist_ok=True, parents=True)
   if corpus_description["build_system"] == "cmake":
     configure_command_vector = cmake_builder.generate_configure_command(
         os.path.join(source_dir, corpus_description["cmake_root"]),
