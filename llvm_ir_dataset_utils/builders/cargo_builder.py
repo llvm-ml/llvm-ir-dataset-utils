1c5a07d8d4485a0b4d9565c9388fc3a6e5835898
Aiden Grossman <agrossman154@yahoo.com>
2023-07-07 21:58:12 -0700

    Return array so other things don't break

--- llvm_ir_dataset_utils/builders/cargo_builder.py
+++ llvm_ir_dataset_utils/builders/cargo_builder.py
@@ -27,7 +27,7 @@ def get_spec_from_id(id):
 def get_packages_from_manifest(source_dir):
   command_vector = ["cargo", "metadata", "--no-deps"]
   if not os.path.exists(source_dir):
-    return
+    return []
   try:
     # TODO(boomanaiden154): Dump the stderr of the metadata command to a log
     # somewhere
