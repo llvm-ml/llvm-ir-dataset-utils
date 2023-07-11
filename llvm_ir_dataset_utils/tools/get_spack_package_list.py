e3cdf4922172453101d313cadb5dad457d471fdd
Aiden Grossman <agrossman154@yahoo.com>
2023-07-11 16:06:44 -0700

    Make get_spack_package_list.py go through all packages
    Before it would only emit all the concretized version (along with
    dependencies) for a single package. This changes that behavior to
    actually write them all to a file and also changes the concretization
    strategy so everything can actually be concretized with clang.
    
    Still some work to do as Spack things some packages that can't be built
    with clang can be built with clang, but we're definitely making some
    progress.

--- llvm_ir_dataset_utils/tools/get_spack_package_list.py
+++ llvm_ir_dataset_utils/tools/get_spack_package_list.py
@@ -8,8 +8,8 @@ default python interpreter.
 import json
 import multiprocessing
 import tempfile
-
-from collections import OrderedDict
+import os
+import pathlib
 
 from absl import app
 from absl import logging
@@ -26,15 +26,17 @@ flags.DEFINE_string('package_list', 'package_list.json',
                     'The path to write the package list to.')
 flags.DEFINE_string(
     'spack_environment', None, 'The path to an existing spack '
-    'environment that has already been concretized.')
+    'environment that has already been concretized or a path to place a new '
+    'spack environment to avoid the length concretization process when running '
+    'multiple times in a row.')
 
 EXCLUDED_PACKAGES = [
     'abi-compliance-checker', 'allpaths-lg', 'fujitsu-fftw', 'cusz',
     'libpressio', 'openspeedshop-utils', 'unblur', 'rstudio',
-    'cbtf-agronavis-gui'
+    'cbtf-agronavis-gui', 'bucky', 'elfutils', 'dyninst', 'openradioss-starter',
+    'cuda', 'root', 'cbench', 'fds', 'kahip', 'kentutils', 'maverick', 'pdc',
+    'plasma', 'clingo-bootstrap'
 ]
-
-EXCLUDED_DEPENDENCIES = ['cuda']
 
 
 def add_concrete_package_and_all_deps(concretized_packages, spec):
@@ -44,7 +46,35 @@ def add_concrete_package_and_all_deps(concretized_packages, spec):
       'name': str(spec.package.fullname.split('.')[1])
   }
   for dep_spec in spec.dependencies():
-    add_concrete_package_and_all_deps(concretized_packages, dep_spec)
+    if dep_spec.dag_hash() not in concretized_packages:
+      add_concrete_package_and_all_deps(concretized_packages, dep_spec)
+
+
+def recursively_check_dependencies(package, should_include_cache):
+  if package.name in should_include_cache:
+    return should_include_cache[package.name]
+  for dependency in package.dependencies.keys():
+    if dependency in should_include_cache and not should_include_cache[
+        dependency]:
+      should_include_cache[package.name] = False
+      return False
+    elif dependency in EXCLUDED_PACKAGES:
+      should_include_cache[package.name] = False
+      return False
+    else:
+      # Try except here because Spack might not be able to find the package
+      # (i.e., if it's a virtual package).
+      try:
+        dependency_pkg_class = spack.repo.path.get_pkg_class(dependency)
+        dependency_pkg = dependency_pkg_class(spack.spec.Spec(dependency))
+        recursive_check_result = recursively_check_dependencies(
+            dependency_pkg, should_include_cache)
+        if not recursive_check_result:
+          return False
+      except:
+        continue
+  should_include_cache[package.name] = True
+  return True
 
 
 def main(_):
@@ -53,10 +83,11 @@ def main(_):
 
   concretized_packages = {}
 
-  if not FLAGS.spack_environment:
+  if not FLAGS.spack_environment or not os.path.exists(FLAGS.spack_environment):
     logging.info('Processing packages.')
 
     package_list = []
+    should_include_cache = {}
 
     for package in packages:
       pkg_class = spack.repo.path.get_pkg_class(package)
@@ -67,24 +98,42 @@ def main(_):
           pkg.build_system_class == 'MakefilePackage' or
           pkg.build_system_class == 'AutotoolsPackage' or
           pkg.build_system_class == 'MesonPackage'):
-        for excluded_dep in EXCLUDED_DEPENDENCIES:
-          if excluded_dep in pkg.dependencies.keys():
-            continue
-        package_list.append(pkg.name)
+        to_add = True
+        for excluded_package in EXCLUDED_PACKAGES:
+          if pkg.name == excluded_package:
+            to_add = False
+            break
+        if to_add:
+          to_add = recursively_check_dependencies(pkg, should_include_cache)
+        if to_add:
+          package_list.append(pkg.name)
 
+    logging.info(f"Concretizing {len(package_list)} packages in environment")
     with tempfile.TemporaryDirectory() as tempdir:
-      env = spack.environment.create_in_dir(tempdir)
-      package_list = ['3dtk']
+      if FLAGS.spack_environment:
+        pathlib.Path(FLAGS.spack_environment).mkdir(
+            exist_ok=False, parents=True)
+        env = spack.environment.create_in_dir(FLAGS.spack_environment)
+      else:
+        env = spack.environment.create_in_dir(tempdir)
       for package in package_list:
         env.add(spack.spec.Spec(package))
-      env.concretize()
+      with spack.config.override('config:build_jobs',
+                                 multiprocessing.cpu_count()):
+        env.unify = False
+        env.concretize()
+      if FLAGS.spack_environment:
+        env.write()
   else:
     logging.info('Loading existing environment')
     env = spack.environment.Environment(FLAGS.spack_environment)
 
+  logging.info('Processing concretized environment.')
   concretized_specs = env.all_specs()
-  add_concrete_package_and_all_deps(concretized_packages, concretized_specs[0])
+  for concretized_spec in concretized_specs:
+    add_concrete_package_and_all_deps(concretized_packages, concretized_spec)
 
+  logging.info(f'Writing {len(concretized_packages)} processed specs to file.')
   with open(FLAGS.package_list, 'w') as package_list_file:
     json.dump(concretized_packages, package_list_file, indent=2)
 
