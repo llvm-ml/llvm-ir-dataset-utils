ae5ccf8ff1ba3f784a1ee6cc4a9fedb6b5550145
Aiden Grossman <agrossman154@yahoo.com>
2023-07-13 21:00:41 -0700

    Add support for getting null build logs

--- llvm_ir_dataset_utils/tools/get_build_failure_logs.py
+++ llvm_ir_dataset_utils/tools/get_build_failure_logs.py
@@ -7,6 +7,7 @@ import json
 
 from absl import app
 from absl import flags
+from absl import logging
 
 FLAGS = flags.FLAGS
 
@@ -17,13 +18,21 @@ flags.mark_flag_as_required('corpus_dir')
 
 
 def main(_):
+  build_failures = 0
+  missing_logs = 0
   for build_manifest_file_path in glob.glob(
       os.path.join(FLAGS.corpus_dir, '*/build_manifest.json')):
     with open(build_manifest_file_path) as build_manifest_file:
       build_manifest = json.load(build_manifest_file)
     for target in build_manifest['targets']:
       if target['success'] == False and target['build_log'] is not None:
-        print(target['name'] + ',' + target['build_log'])
+        print(target['name'] + ',failure,' + target['build_log'])
+        build_failures += 1
+      if target['build_log'] is None:
+        print(target['name'] + ',no_logs,NULL')
+        missing_logs += 1
+  logging.warning(f'Found {build_failures} build failures.')
+  logging.warning(f'{missing_logs} targets were missing logs.')
 
 
 if __name__ == '__main__':
