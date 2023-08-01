2fe31caf72af27b222242144f5e614a684a9b5b7
Aiden Grossman <agrossman154@yahoo.com>
2023-07-31 23:35:35 -0700

    Add function to get which passes changed the IR

--- llvm_ir_dataset_utils/util/bitcode_module.py
+++ llvm_ir_dataset_utils/util/bitcode_module.py
@@ -30,3 +30,28 @@ def extract_functions(bitcode_module_path, extraction_path):
     ]
     subprocess.run(extract_command_vector, check=True)
 
+
+def get_run_passes_opt(bitcode_function_path):
+  opt_command_vector = [
+      'opt', bitcode_function_path, '-print-changed', '-passes=default<O3>',
+      '-o', '/dev/null'
+  ]
+  opt_process = subprocess.run(
+      opt_command_vector,
+      encoding='UTF-8',
+      stdout=subprocess.DEVNULL,
+      stderr=subprocess.PIPE,
+      check=True)
+  opt_process_lines = opt_process.stderr.split('\n')
+  passes = []
+  for opt_process_line in opt_process_lines:
+    if opt_process_line[:3] == '***' and opt_process_line[-3:] == '***':
+      # We're in a pass status line
+      if opt_process_line[4:11] == 'IR Pass':
+        # All module level passes are ignored, so we can't do anything here.
+        continue
+      if opt_process_line[-13:-4] == 'no change':
+        passes.append((opt_process_line.split(' ')[4], False))
+      else:
+        passes.append((opt_process_line.split(' ')[4], True))
+  return passes
