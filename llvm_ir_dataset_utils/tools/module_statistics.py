f3771d89c5aee3741ee4fd7ac56ffcd052ab36dd
Aiden Grossman <agrossman154@yahoo.com>
2023-08-01 19:32:57 -0700

    Parallelize module_statistics.py
    Is pretty much necessary to get any reasonable throughput for collecting
    pass pipeline statistics.

--- llvm_ir_dataset_utils/tools/module_statistics.py
+++ llvm_ir_dataset_utils/tools/module_statistics.py
@@ -8,30 +8,54 @@ import csv
 from absl import app
 from absl import flags
 
+import ray
+
 from llvm_ir_dataset_utils.util import bitcode_module
 
 FLAGS = flags.FLAGS
 
 # TODO(boomanaiden154): Refactor this to a corpus directory, maybe with a sampling/filtering
 # clause to allow for smaller scale testing.
-flags.DEFINE_string('project_dir', None,
+flags.DEFINE_string('corpus_dir', None,
                     'The corpus directory to look for modules in.')
 flags.DEFINE_string('output_file_path', None, 'The output file.')
 
-flags.mark_flag_as_required('project_dir')
+flags.mark_flag_as_required('corpus_dir')
 flags.mark_flag_as_required('output_file_path')
 
 
-def main(_):
+@ray.remote
+def process_single_project(project_dir):
   passes_changed = {}
   for bitcode_file_path in glob.glob(
-      os.path.join(FLAGS.project_dir, '**/*.bc'), recursive=True):
-    bitcode_file_full_path = os.path.join(FLAGS.project_dir, bitcode_file_path)
-    logging.info(f'processing {bitcode_file_full_path}')
+      os.path.join(project_dir, '**/*.bc'), recursive=True):
+    bitcode_file_full_path = os.path.join(project_dir, bitcode_file_path)
     modules_passes_ran = bitcode_module.get_passes_bitcode_module(
         bitcode_file_full_path)
     passes_changed = bitcode_module.combine_module_passes(
         passes_changed, modules_passes_ran)
+  return passes_changed
+
+
+def main(_):
+  ray.init()
+
+  projects = os.listdir(FLAGS.corpus_dir)
+  project_futures = []
+
+  for project_dir in projects:
+    full_project_path = os.path.join(FLAGS.corpus_dir, project_dir)
+    project_futures.append(process_single_project.remote(full_project_path))
+
+  passes_changed = {}
+
+  while len(project_futures) > 0:
+    finished, project_futures = ray.wait(project_futures, timeout=5.0)
+    logging.info(
+        f'Just finished {len(finished)}, {len(project_futures)} remaining.')
+    for change_info in ray.get(finished):
+      passes_changed = bitcode_module.combine_module_passes(
+          passes_changed, change_info)
 
   with open(FLAGS.output_file_path, 'w') as output_file:
     csv_writer = csv.writer(output_file)
