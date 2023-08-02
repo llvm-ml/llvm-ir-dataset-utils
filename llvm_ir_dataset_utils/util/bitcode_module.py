498668d36d22a949ebc5e5368b53a645618a368f
Aiden Grossman <agrossman154@yahoo.com>
2023-08-02 15:21:41 -0700

    Increase per-project parallelism for statistics gathering

--- llvm_ir_dataset_utils/util/bitcode_module.py
+++ llvm_ir_dataset_utils/util/bitcode_module.py
@@ -5,6 +5,10 @@ import os
 import tempfile
 import logging
 
+import ray
+
+BITCODE_FILE_CHUNK_SIZE = 16
+
 
 def get_function_symbols(bitcode_module):
   # TODO(boomanaiden154): Adjust after symlinking to llvm-nm
@@ -31,23 +35,33 @@ def get_function_symbols(bitcode_module):
   return module_list
 
 
-def extract_functions(bitcode_module, extraction_path):
-  function_symbols_list = get_function_symbols(bitcode_module)
+def extract_individual_function(bitcode_module, extraction_path,
+                                function_symbol):
+  function_module_name = os.path.join(extraction_path, f'{function_symbol}.bc')
+  # TODO(boomanaiden154): Adjust after symlinking to llvm-extract
+  extract_command_vector = [
+      'llvm-extract-16', '-func', function_symbol, '-o', function_module_name
+  ]
+  with subprocess.Popen(
+      extract_command_vector,
+      stderr=subprocess.DEVNULL,
+      stdout=subprocess.DEVNULL,
+      stdin=subprocess.PIPE) as extraction_process:
+    extraction_process.communicate(input=bitcode_module)
+    if extraction_process.returncode != 0:
+      logging.info(f'Failed to extract {function_symbol}')
+      return []
+
+  return [function_module_name]
+
+
+def extract_functions(bitcode_module, extraction_path, function_symbols_list):
+  extracted_functions = []
   for function_symbol in function_symbols_list:
-    function_module_name = os.path.join(extraction_path,
-                                        f'{function_symbol}.bc')
-    # TODO(boomanaiden154): Adjust after symlinking to llvm-extract
-    extract_command_vector = [
-        'llvm-extract-16', '-func', function_symbol, '-o', function_module_name
-    ]
-    with subprocess.Popen(
-        extract_command_vector,
-        stderr=subprocess.DEVNULL,
-        stdout=subprocess.DEVNULL,
-        stdin=subprocess.PIPE) as extraction_process:
-      extraction_process.communicate(input=bitcode_module)
-      if extraction_process.returncode != 0:
-        logging.info(f'Failed to extract {function_symbol}')
+    extracted_functions.extend(
+        extract_individual_function(bitcode_module, extraction_path,
+                                    function_symbol))
+  return extracted_functions
 
 
 def get_run_passes_opt(bitcode_function_path):
@@ -119,19 +133,52 @@ def get_function_properties(bitcode_function_path):
   return properties_dict
 
 
+@ray.remote(num_cpus=1)
+def get_statistics_batch(bitcode_module, function_symbols, statistics_type):
+  statistics = {}
+  with tempfile.TemporaryDirectory() as extracted_functions_dir:
+    bitcode_function_paths = extract_functions(bitcode_module,
+                                               extracted_functions_dir,
+                                               function_symbols)
+    for bitcode_function_path in bitcode_function_paths:
+      if statistics_type == 'properties':
+        function_statistics = get_function_properties(bitcode_function_path)
+      elif statistics_type == 'passes':
+        function_statistics = get_run_passes_opt(bitcode_function_path)
+      statistics = combine_module_statistics(statistics, function_statistics)
+  return statistics
+
+
+# TODO(boomanaiden154): Probably put this into a separate parallel util module
+# or something.
+# TODO(boomanaiden154): Write some unit tests for this function.
+def split_batches(individual_jobs, batch_size):
+  batches = []
+  current_start_index = 0
+  while True:
+    end_index = current_start_index + batch_size
+    chunk = individual_jobs[current_start_index:end_index]
+    batches.append(chunk)
+    current_start_index = end_index
+    if current_start_index + batch_size >= len(individual_jobs):
+      last_chunk = individual_jobs[current_start_index:]
+      batches.append(last_chunk)
+      break
+  return batches
+
+
 def get_bitcode_module_statistics(bitcode_module, statistics_type):
   with tempfile.TemporaryDirectory() as extracted_functions_dir:
-    extract_functions(bitcode_module, extracted_functions_dir)
-    function_bitcode_files = os.listdir(extracted_functions_dir)
-    function_passes = {}
-    for function_bitcode_file in function_bitcode_files:
-      full_bitcode_file_path = os.path.join(extracted_functions_dir,
-                                            function_bitcode_file)
-      if statistics_type == 'properties':
-        current_function_results = get_function_properties(
-            full_bitcode_file_path)
-      elif statistics_type == 'passes':
-        current_function_results = get_run_passes_opt(full_bitcode_file_path)
-      function_passes = combine_module_statistics(function_passes,
-                                                  current_function_results)
-  return function_passes
+    function_symbols = get_function_symbols(bitcode_module)
+
+    statistics_futures = []
+    batches = split_batches(function_symbols, BITCODE_FILE_CHUNK_SIZE)
+    for batch in batches:
+      statistics_futures.append(
+          get_statistics_batch.remote(bitcode_module, batch, statistics_type))
+
+    statistics_chunks = ray.get(statistics_futures)
+    statistics = {}
+    for statistics_chunk in statistics_chunks:
+      statistics = combine_module_statistics(statistics, statistics_chunk)
+  return statistics
