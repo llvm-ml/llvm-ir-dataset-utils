b8f147e7ef78278be8871529d7eadcdebe910a86
Aiden Grossman <agrossman154@yahoo.com>
2023-08-01 22:58:41 -0700

    Make get_build_failure_logs.py work with archived corpora
    This is necessary to support builds like spack that are now done
    exclusively with each project corpus getting archived to make sure we
    hit the parallel file system inode limit.

--- llvm_ir_dataset_utils/tools/get_build_failure_logs.py
+++ llvm_ir_dataset_utils/tools/get_build_failure_logs.py
@@ -9,6 +9,10 @@ import logging
 from absl import app
 from absl import flags
 
+import ray
+
+from llvm_ir_dataset_utils.util import dataset_corpus
+
 FLAGS = flags.FLAGS
 
 flags.DEFINE_string('corpus_dir', None,
@@ -17,20 +21,47 @@ flags.DEFINE_string('corpus_dir', None,
 flags.mark_flag_as_required('corpus_dir')
 
 
+@ray.remote(num_cpus=1)
+def process_corpus(build_corpus_path):
+  build_manifest = dataset_corpus.load_json_from_corpus(
+      build_corpus_path, './build_manifest.json')
+  if build_manifest is None:
+    return None
+  for target in build_manifest['targets']:
+    if target['success'] == False and target['build_log'] is not None:
+      return ('build_failure', target['name'], target['build_log'])
+    if target['build_log'] is None:
+      return ('missing_logs', target['name'], None)
+  return None
+
+
 def main(_):
+  ray.init()
+
+  build_corpora = os.listdir(FLAGS.corpus_dir)
+  corpus_futures = []
+
+  for build_corpus in build_corpora:
+    corpus_path = os.path.join(FLAGS.corpus_dir, build_corpus)
+    corpus_futures.append(process_corpus.remote(corpus_path))
+
   build_failures = 0
   missing_logs = 0
-  for build_manifest_file_path in glob.glob(
-      os.path.join(FLAGS.corpus_dir, '*/build_manifest.json')):
-    with open(build_manifest_file_path) as build_manifest_file:
-      build_manifest = json.load(build_manifest_file)
-    for target in build_manifest['targets']:
-      if target['success'] == False and target['build_log'] is not None:
-        print(target['name'] + ',failure,' + target['build_log'])
-        build_failures += 1
-      if target['build_log'] is None:
-        print(target['name'] + ',no_logs,NULL')
-        missing_logs += 1
+
+  while len(corpus_futures) > 0:
+    finished, corpus_futures = ray.wait(corpus_futures, timeout=5.0)
+    finished_data = ray.get(finished)
+    logging.info(
+        f'Just finished {len(finished)}, {len(corpus_futures)} remaining.')
+    for finished_corpus in finished_data:
+      if finished_corpus is not None:
+        if finished_corpus[0] == 'build_failure':
+          build_failures += 1
+          print(f'{finished_corpus[1]},failure,{finished_corpus[2]}')
+        elif finished_corpus[0] == 'missing_logs':
+          missing_logs += 1
+          print(f'{finished_corpus[1]},no_logs,NULL')
+
   logging.warning(f'Found {build_failures} build failures.')
   logging.warning(f'{missing_logs} targets were missing logs.')
 
