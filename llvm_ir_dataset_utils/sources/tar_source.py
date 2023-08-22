6d508022f31843193ac132676701273de2b15854
Aiden Grossman <agrossman154@yahoo.com>
2023-08-22 14:03:15 -0700

    Switched tar source to requests for SSL reasons

--- llvm_ir_dataset_utils/sources/tar_source.py
+++ llvm_ir_dataset_utils/sources/tar_source.py
@@ -5,18 +5,17 @@ import tarfile
 import tempfile
 import shutil
 import logging
-from urllib import request
+import requests
+import io
 
 
 def download_source_code(archive_url, base_dir, source_folder_name):
   try:
     with tempfile.TemporaryDirectory() as download_dir:
-      file_download_path = os.path.join(download_dir,
-                                        'tar-source-download.tar.gz')
-      request.urlretrieve(archive_url, file_download_path)
-      with tarfile.open(file_download_path) as source_tar_archive:
+      tar_archive = requests.get(archive_url, verify=False)
+      tar_archive_file = io.BytesIO(tar_archive.content)
+      with tarfile.open(fileobj=tar_archive_file) as source_tar_archive:
         source_tar_archive.extractall(download_dir)
-      os.remove(file_download_path)
       real_source_folder_name = os.path.join(download_dir,
                                              os.listdir(download_dir)[0])
       shutil.move(real_source_folder_name,
