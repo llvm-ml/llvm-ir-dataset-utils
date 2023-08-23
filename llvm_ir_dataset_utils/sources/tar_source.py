d24a05ba6fdc8527b9ac043c98c112f8abd40f4f
Aiden Grossman <agrossman154@yahoo.com>
2023-08-22 22:14:32 -0700

    Hopefully fixed another failure case and disabled SSL warnings

--- llvm_ir_dataset_utils/sources/tar_source.py
+++ llvm_ir_dataset_utils/sources/tar_source.py
@@ -7,20 +7,28 @@ import shutil
 import logging
 import requests
 import io
+import urllib3
 
 
 def download_source_code(archive_url, base_dir, source_folder_name):
+  # Disable warnings, otherwise we get a lot of warnings about disabling SSL
+  # verification.
+  urllib3.disable_warnings()
   try:
     with tempfile.TemporaryDirectory() as download_dir:
       tar_archive = requests.get(archive_url, verify=False)
       tar_archive_file = io.BytesIO(tar_archive.content)
       with tarfile.open(fileobj=tar_archive_file) as source_tar_archive:
         source_tar_archive.extractall(download_dir)
-      real_source_folder_name = os.path.join(download_dir,
-                                             os.listdir(download_dir)[0])
-      shutil.move(real_source_folder_name,
-                  os.path.join(base_dir, source_folder_name))
-    success = True
+      download_dir_files = os.listdir(download_dir)[0]
+      if len(download_dir_files) != 0:
+        real_source_folder_name = os.path.join(download_dir,
+                                               os.listdir(download_dir)[0])
+        shutil.move(real_source_folder_name,
+                    os.path.join(base_dir, source_folder_name))
+        success = True
+      else:
+        success = False
   except (EOFError, OSError):
     logging.warning(f'Downloading tar archive {archive_url} failed.')
     success = False
