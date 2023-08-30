fd15055d4751cf15aa39eff9b5d458c36e066ca3
Aiden Grossman <agrossman154@yahoo.com>
2023-08-30 00:17:54 -0700

    Add support for multiple languages in generate_histogram.py

--- llvm_ir_dataset_utils/visualization_tools/generate_histograms.py
+++ llvm_ir_dataset_utils/visualization_tools/generate_histograms.py
@@ -14,7 +14,7 @@ from absl import flags
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_string('data_path', None, 'The path to the data file.')
+flags.DEFINE_multi_string('data_path', None, 'The path to the data file.')
 flags.DEFINE_string('output_path', None,
                     'The path to a folder to write the histograms to.')
 flags.DEFINE_integer('num_bins', 20,
@@ -29,9 +29,18 @@ flags.mark_flag_as_required('output_path')
 
 
 def main(_):
-  logging.info('Loading data.')
-  data_frame = pandas.read_csv(FLAGS.data_path)
-  data_frame.drop(['name'], axis=1, inplace=True)
+  data_frames = []
+  languages = []
+
+  for data_path in FLAGS.data_path:
+    data_frame = pandas.read_csv(data_path)
+    data_frame.drop(['name'], axis=1, inplace=True)
+    language_name = os.path.basename(data_path)[:-4]
+    languages.append(language_name)
+    data_frame.insert(0, 'language', [language_name] * len(data_frame))
+    data_frames.append(data_frame)
+
+  data_frame = pandas.concat(data_frames)
 
   for column in data_frame:
     print(column)
@@ -41,23 +50,35 @@ def main(_):
   if FLAGS.sub_plot_sections is None:
     for column in data_frame:
       figure = plotly.express.histogram(
-          data_frame[column].to_numpy(), nbins=FLAGS.num_bins, log_y=True)
+          data_frame,
+          x=column,
+          color='language',
+          nbins=FLAGS.num_bins,
+          log_y=True,
+          barmode='overlay')
       figure.write_image(os.path.join(FLAGS.output_path, f'{column}.png'))
+      logging.info(f'Finished generating figure for {column}')
+    return
 
+  # TODO(boomanaiden154): More elegant support for multiple languages
+  # with a subplot figure.
   subplot_figure = fig = plotly.subplots.make_subplots(
       rows=2, cols=4, subplot_titles=FLAGS.sub_plot_sections)
 
   for index, sub_plot_section in enumerate(FLAGS.sub_plot_sections):
     column = (index % 4) + 1
     row = int(index / 4 + 1)
-    subplot_figure.add_trace(
-        plotly.graph_objects.Histogram(
-            x=data_frame[sub_plot_section].to_numpy(),
-            nbinsx=FLAGS.num_bins,
-            name=sub_plot_section),
-        col=column,
-        row=row)
-    subplot_figure.update_yaxes(type="log", col=column, row=row)
+
+    for language in languages:
+      data_frame_subset = data_frame[data_frame['language'] == language]
+      subplot_figure.add_trace(
+          plotly.graph_objects.Histogram(
+              x=data_frame_subset[sub_plot_section].to_numpy(),
+              nbinsx=FLAGS.num_bins,
+              name=language),
+          col=column,
+          row=row)
+      subplot_figure.update_yaxes(type="log", col=column, row=row)
 
   subplot_figure.update_layout(width=2200, height=1000)
 
