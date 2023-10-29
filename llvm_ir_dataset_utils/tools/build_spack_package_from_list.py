42d2e9eda2b0c4dd9406c758158708334604ecce
Aiden Grossman <agrossman154@yahoo.com>
2023-10-28 21:56:48 -0700

    Add license propogation to spack builder

--- llvm_ir_dataset_utils/tools/build_spack_package_from_list.py
+++ llvm_ir_dataset_utils/tools/build_spack_package_from_list.py
@@ -56,6 +56,7 @@ def get_package_future(package_dict, current_package_futures, package, threads):
       'package_name': package_dict[package]['name'],
       'package_spec': package_dict[package]['spec'],
       'package_hash': package,
+      'license': package_dict[package]['license'],
       'sources': []
   }
   extra_builder_arguments = {
