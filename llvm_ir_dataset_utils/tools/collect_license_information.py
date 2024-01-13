fd983bfa8108451c5a8a999593e0527faf8bddb2
Aiden Grossman <agrossman154@yahoo.com>
2024-01-13 15:28:54 -0800

    Add tool for collecting license information

---
+++ llvm_ir_dataset_utils/tools/collect_license_information.py
@@ -0,0 +1,57 @@
+"""Tool for collecting license information on all projects and putting it into a CSV
+file.
+"""
+
+import os
+import logging
+import json
+
+from absl import flags
+from absl import app
+
+import ray
+
+from llvm_ir_dataset_utils.util import dataset_corpus
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string('corpus_dir', None, 'The base directory of the corpus')
+flags.DEFINE_string('output_file', None, 'The path to the output JSON file.')
+
+flags.mark_flag_as_required('corpus_dir')
+flags.mark_flag_as_required('output_file')
+
+
+@ray.remote(num_cpus=1)
+def get_license_information(corpus_path):
+  build_manifest = dataset_corpus.load_json_from_corpus(
+      corpus_path, './build_manifest.json')
+
+  if build_manifest is None:
+    return None
+
+  return (corpus_path, build_manifest['license'],
+          build_manifest['license_source'], build_manifest["license_files"])
+
+
+def main(_):
+  build_corpora = os.listdir(FLAGS.corpus_dir)
+
+  license_info_futures = []
+  for build_corpus in build_corpora:
+    corpus_path = os.path.join(FLAGS.corpus_dir, build_corpus)
+    license_info_futures.append(get_license_information.remote(corpus_path))
+
+  raw_license_information = ray.get(license_info_futures)
+
+  license_information = [
+      license_info for license_info in raw_license_information
+      if license_info is not None
+  ]
+
+  with open(FLAGS.output_file, 'w') as output_json_file:
+    json.dump(license_information, output_json_file, indent=4)
+
+
+if __name__ == '__main__':
+  app.run(main)
