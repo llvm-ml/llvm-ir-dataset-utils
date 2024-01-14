9f73c6bf280a3a3ba0fae675cfdda73492d2cb77
Aiden Grossman <agrossman154@yahoo.com>
2024-01-14 03:54:30 -0800

    Make data uploads in parallel

--- llvm_ir_dataset_utils/tools/upload_dataset_hf.py
+++ llvm_ir_dataset_utils/tools/upload_dataset_hf.py
@@ -8,7 +8,12 @@ import os
 from absl import app
 from absl import flags
 
+import ray
+
 from huggingface_hub import HfApi
+from huggingface_hub import CommitOperationAdd
+from huggingface_hub import preupload_lfs_files
+from huggingface_hub import create_commit
 
 FLAGS = flags.FLAGS
 
@@ -19,21 +24,67 @@ flags.DEFINE_string('start_after', None, 'A specific path to start at.')
 flags.mark_flag_as_required('dataset_dir')
 
 
+@ray.remote(num_cpus=4)
+def upload_file(api, full_file_path, file_to_upload):
+  try:
+    operation = CommitOperationAdd(
+        path_in_repo=file_to_upload, path_or_fileobj=full_file_path)
+    preupload_lfs_files(
+        'llvm-ml/ComPile', additions=[operation], repo_type='dataset')
+    logging.warning(f'Finished uploading {file_to_upload}')
+    return (True, operation)
+  except Exception as e:
+    logging.error(f'Ran into an error, retrying {file_to_upload}: {e}')
+    return (False, full_file_path, file_to_upload)
+
+
 def main(_):
   logging.info('Starting the upload')
   api = HfApi()
+
+  file_upload_futures = []
+
   for file_to_upload in os.listdir(FLAGS.dataset_dir):
     if FLAGS.start_after and file_to_upload <= FLAGS.start_after:
       logging.info(f'Skipping uploading {file_to_upload}')
       continue
 
     full_file_path = os.path.join(FLAGS.dataset_dir, file_to_upload)
-    api.upload_file(
-        path_or_fileobj=full_file_path,
-        path_in_repo=file_to_upload,
-        repo_id='llvm-ml/ComPile',
-        repo_type='dataset')
-    logging.info(f'Finished uploading {file_to_upload}')
+    file_upload_futures.append(
+        upload_file.remote(api, full_file_path, file_to_upload))
+
+  current_operations = []
+
+  while len(file_upload_futures) > 0:
+    completed_uploads, file_upload_futures = ray.wait(
+        file_upload_futures, timeout=5)
+
+    logging.info(
+        f'Just finished {len(completed_uploads)}, {len(file_upload_futures)} remaining.'
+    )
+    returned_uploads = ray.get(completed_uploads)
+
+    for returned_upload in returned_uploads:
+      if returned_upload[0]:
+        current_operations.append(returned_upload[1])
+      else:
+        file_upload_futures.append(
+            upload_file.remote(api, returned_upload[0], returned_upload[1]))
+
+    if len(current_operations) > 50:
+      create_commit(
+          'llvm-ml/ComPile',
+          operations=current_operations,
+          commit_message='Add additional data',
+          repo_type='dataset')
+      current_operations = []
+
+  create_commit(
+      'llvm-ml/ComPile',
+      operations=current_operations,
+      commit_message='Add additional data',
+      repo_type='dataset')
+  current_operations = []
 
 
 if __name__ == '__main__':
