9e8580789b02d0ab45129a145c190b9a07c977cc
Aiden Grossman <agrossman154@yahoo.com>
2024-01-13 21:41:13 -0800

    Add more fields into parquet files

--- llvm_ir_dataset_utils/tools/process_to_parquet.py
+++ llvm_ir_dataset_utils/tools/process_to_parquet.py
@@ -5,6 +5,7 @@ into a parquet dataset for distribution.
 import logging
 import os
 import sys
+import glob
 
 from absl import app
 from absl import flags
@@ -20,29 +21,46 @@ from llvm_ir_dataset_utils.util import dataset_corpus
 FLAGS = flags.FLAGS
 
 flags.DEFINE_string('corpus_dir', None, 'The corpus to pull bitcode from.')
-flags.DEFINE_integer('max_projects', sys.maxsize,
+flags.DEFINE_integer('max_batches', sys.maxsize,
                      'The maximum number of projects to process')
 
 flags.mark_flag_as_required('corpus_dir')
 
-# TODO(boomanaiden154): Add in support for propogating license information
-# and other project provenance information once we have it.
 
-
-def process_single_project(project_dir, dataset_dir):
+def process_single_batch(batch_dir, dataset_dir):
   try:
-    bitcode_paths = dataset_corpus.get_bitcode_file_paths(project_dir)
+    bitcode_paths = dataset_corpus.get_bitcode_file_paths(batch_dir)
+    license_information = dataset_corpus.load_json_from_corpus(
+        batch_dir, './license_info.json')
   except:
+    logging.warning('Failed to get bitcode_paths')
     return
-
   module_content = []
+  license_expression = []
+  license_source = []
+  license_file = []
+  package_source = []
 
   for bitcode_path in bitcode_paths:
     bitcode_file_data = dataset_corpus.load_file_from_corpus(
-        project_dir, bitcode_path)
+        batch_dir, bitcode_path)
     module_content.append(bitcode_file_data)
 
-  dataframe = pandas.DataFrame.from_dict({'content': module_content})
+    # Cut off the first two characters and the last two characters as we only
+    # want the raw module hash.
+    bitcode_license_info = license_information[bitcode_path[2:-3]]
+    license_expression.append(bitcode_license_info[0])
+    license_source.append(bitcode_license_info[1])
+    license_file.append(bitcode_license_info[2])
+    package_source.append(bitcode_license_info[3])
+
+  dataframe = pandas.DataFrame.from_dict({
+      'content': module_content,
+      'license_expression': license_expression,
+      'license_source': license_source,
+      'license_files': license_file,
+      'package_source': package_source
+  })
 
   table = pyarrow.Table.from_pandas(dataframe, preserve_index=False)
 
@@ -55,11 +73,11 @@ def main(_):
   logging.info(f'Processing {len(projects_list)} projects')
 
   for index, project_dir in enumerate(projects_list):
-    project_path = os.path.join(FLAGS.corpus_dir, project_dir)
-    process_single_project(project_path, '/tmp/test.parquet')
+    batch_path = os.path.join(FLAGS.corpus_dir, project_dir)
+    process_single_batch(batch_path, '/tmp/test.parquet')
     logging.info(f'Just finished processing {project_dir}')
 
-    if index >= FLAGS.max_projects:
+    if index >= FLAGS.max_batches:
       break
 
 
