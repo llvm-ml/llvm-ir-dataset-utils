f4ca68a910c40e419bf782201a66d374a8c5f952
Aiden Grossman <agrossman154@yahoo.com>
2024-03-25 00:58:57 -0700

    Make get_bbs script work with new bb extraction utility

--- llvm_ir_dataset_utils/tools/get_bbs.py
+++ llvm_ir_dataset_utils/tools/get_bbs.py
@@ -32,7 +32,7 @@ PROJECT_MODULE_CHUNK_SIZE = 8
 
 
 def get_basic_blocks(input_file_path, module_id):
-  command_vector = ['PrintBasicBlocks', input_file_path]
+  command_vector = ['extract_bbs_from_obj', input_file_path]
   command_output = subprocess.run(
       command_vector, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   if (command_output.returncode != 0):
@@ -166,6 +166,8 @@ def main(_):
   for corpus_dir in FLAGS.corpus_dir:
     for project_dir in os.listdir(corpus_dir):
       project_dirs.append(os.path.join(corpus_dir, project_dir))
+
+  get_bbs_from_projects(project_dirs, FLAGS.output_file)
 
 
 if __name__ == '__main__':
